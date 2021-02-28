cf1fed7c8caf5946b34782cc44f28754c9e03efe
roya.salehi <roya.salehi@aut.ac.ir>
2021-02-28 22:32:41 +0330

    r

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Date;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+      System.out.println(args[0]);
+      System.out.println(args[0]);
+
+    }
+}
